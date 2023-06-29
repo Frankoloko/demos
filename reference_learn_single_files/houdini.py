8dc0d82077c1493d036c3e8fd1633c5fb71036ab
Francois Kruger <francoiskrugermail@gmail.com>
2023-06-29 10:51:02 -0400

    x

---
+++ reference_learn_single_files/houdini.py
@@ -0,0 +1,19 @@
+import hou
+
+############################################################################################################
+# ADDUIBNG
+
+# Get the HDA node
+hda_node = hou.node("/path/to/your/HDA")
+
+# Add an parm
+new_parm = hou.StringParmTemplate("my_int_property", "My Int Property", 1)
+
+# If you want it to get placed at a certain position, use this
+parm_group = hda_node.parmTemplateGroup()
+existing_parm = parm_group.find("some_parm")
+parm_group.insertBefore(existing_parm, new_parm)
+hda_node.setParmTemplateGroup(parm_group)
+
+# If you don't care about the position and you just want it to get added to the bottom of the list then just use this
+hda_node.addSpareParmTuple(new_parm)
